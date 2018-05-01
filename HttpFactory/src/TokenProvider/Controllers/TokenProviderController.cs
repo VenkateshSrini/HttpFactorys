96d650320445e0518166b40ed8850c45cf144dcb
VenkateshSrinivasan <venkateshsrini3@gmail.com>
2018-05-01 22:05:00 +0530

    Base commit

---
+++ HttpFactory/src/TokenProvider/Controllers/TokenProviderController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TokenProvider.Controllers
+{
+  
+    [Route("api/TokenProvider")]
+    public class TokenProviderController : Controller
+    {
+        public IActionResult Get()
+        {
+            return Ok(Guid.NewGuid().ToString());
+        }
+    }
+}
---
+++ HttpFactory/src/TokenProvider/Controllers/ValuesController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TokenProvider.Controllers
+{
+    //[Produces("application/json")]
+    [Route("api/Values")]
+    public class ValuesController : Controller
+    {
+        public IActionResult Get()
+        {
+            if (Request.Headers.ContainsKey("x-access"))
+            {
+                return Ok(new { statusCode = "success" });
+            }
+            else
+                return Unauthorized();
+        }
+    }
+}
